153dcb4b85d0bfc6e7bba3d18a3e889753831efe
Rupak143 <122255423+Rupak143@users.noreply.github.com>
2024-03-29 09:24:46 +0530

    Update 39. Assignment problem using branch and bound algorithm.py

--- 39. Assignment problem using branch and bound algorithm.py
+++ 39. Assignment problem using branch and bound algorithm.py
@@ -1,36 +1,37 @@
-import numpy as np
-from itertools import permutations
+import sys
 def reduce_matrix(matrix):
-    row_min = np.min(matrix, axis=1)
-    matrix -= row_min[:, np.newaxis]
-    col_min = np.min(matrix, axis=0)
-    matrix -= col_min
-    return matrix
-def cost_of_assignment(assignment, cost_matrix):
-    total_cost = 0
-    for i, j in enumerate(assignment):
-        total_cost += cost_matrix[i, j]
-    return total_cost
-def assign_jobs(cost_matrix):
-    n = cost_matrix.shape[0]
-    min_cost = float('inf')
-    min_assignment = None
-    reduced_matrix = reduce_matrix(cost_matrix)
-    unassigned_jobs = list(range(n))
-    for perm in permutations(range(n)):
-        assignment = []
-        total_cost = 0
+    n = len(matrix)
+    for i in range(n):
+        min_val = min(matrix[i])
+        matrix[i] = [val - min_val for val in matrix[i]]
+    for j in range(n):
+        min_val = min(matrix[i][j] for i in range(n))
         for i in range(n):
-            total_cost += reduced_matrix[i, perm[i]]
-            assignment.append((i, perm[i]))
-        if total_cost < min_cost:
-            min_cost = total_cost
-            min_assignment = assignment
+            matrix[i][j] -= min_val         
+    return matrix
+def branch_and_bound_assignment(cost_matrix):
+    n = len(cost_matrix)
+    min_cost = sys.maxsize
+    min_assignment = None
+    def backtrack(assignment, total_cost, assigned_rows):
+        nonlocal min_cost, min_assignment
+        if len(assignment) == n:
+            if total_cost < min_cost:
+                min_cost = total_cost
+                min_assignment = assignment.copy()
+            return
+        for col in range(n):
+            if col not in assigned_rows:
+                potential_cost = total_cost + cost_matrix[len(assignment)][col]
+                if potential_cost < min_cost:
+                    backtrack(assignment + [(len(assignment), col)], potential_cost, assigned_rows + [col])
+    reduced_matrix = reduce_matrix(cost_matrix)
+    backtrack([], 0, [])
     return min_assignment, min_cost
-cost_matrix = np.array([[5, 4, 2, 7],
-                        [2, 1, 5, 6],
-                        [4, 3, 2, 1],
-                        [3, 2, 1, 4]])
-assignment, min_cost = assign_jobs(cost_matrix)
+cost_matrix = [[5, 4, 2, 7],
+               [2, 1, 5, 6],
+               [4, 3, 2, 1],
+               [3, 2, 1, 4]]
+assignment, min_cost = branch_and_bound_assignment(cost_matrix)
 print("Minimum cost of assignment:", min_cost)
 print("Assignment:", assignment)
